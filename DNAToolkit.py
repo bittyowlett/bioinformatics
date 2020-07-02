6484f6406a37f7328bc400e7288434e3d712b81b
bittyowlett <bhavnakandra1412@gmail.com>
2020-07-02 13:09:57 +0530

    till lec 4

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,5 +1,6 @@
 # DNA Toolkit file
 from structures import *
+from collections import Counter
 
 # Checking an input sequence whether it is a valid DNA string or not
 def validateSeq(dna_seq):
@@ -26,4 +27,41 @@ def transcription(seq):
 # Reverse Complimenting 
 def reverse_compliment(seq):
     """ Swaping A with T and G with C and vice versa """
-    return ''.join([DNA_ReverseCompliment[nuc] for nuc in seq])[::-1]
+    #return ''.join([DNA_ReverseCompliment[nuc] for nuc in seq])[::-1]
+
+    # Pythonic Approach: FASTER**
+    mapping = str.maketrans('ATGC', 'TACG')
+    return seq.translate(mapping)[::-1]
+
+
+def gc_content(seq):
+    """GC Content in a DNA/RNA sequence"""
+    return round((seq.count('C') + seq.count('G')) / len(seq) * 100)
+
+
+def gc_content_subsec(seq, k=20):
+    """ GC Content in a DNA/RNA sub-sequence of length k with k = 20 as default value"""
+    res = []
+    for i in range(0,len(seq)-k+1, k):
+        subseq = seq[i:i+k]
+        res.append(gc_content(subseq))
+    return res
+
+
+def translate_seq(seq, init_pos=0):
+    """Translates a DNA sequence into an aminoacid sequence"""
+    return [DNA_Codons[seq[pos:pos+3]] for pos in range(init_pos, len(seq)-2, 3)]
+
+
+def codon_usage(seq, aminoacid):
+    """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+    tmpList = []
+    for i in range(0, len(seq)-2, 3):
+        if DNA_Codons[seq[i:i+3]] == aminoacid:
+            tmpList.append(seq[i:i+3])
+
+    freqDict = dict(Counter(tmpList))
+    totalWeight = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round(freqDict[seq] / totalWeight, 2)
+    return freqDict         
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 # Creating a random DNA sequence for testing
 randDNAStr = ''.join([random.choice(Nucleotides)
-                    for nuc in range(20)])
+                    for nuc in range(50)])
 
 
 DNAStr = validateSeq(randDNAStr)
@@ -14,3 +14,7 @@ print(f'[1] + Sequence Length: {len(DNAStr)}\n')
 print(f'[2] + Nucleotide Frequency: {countNucFreq(DNAStr)}\n')
 print(f'[3] + DNA/RNA Transcription: {transcription(DNAStr)}\n')
 print(f'[4] + Reverse compliment: {reverse_compliment(DNAStr)}\n')
+print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
+print(f'[6] + GC Content in subsection k=5: {gc_content_subsec(DNAStr,5)}\n')
+print(f'[7] + Aminoacid sequence from DNA: {translate_seq(DNAStr,0)}\n')
+print(f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
